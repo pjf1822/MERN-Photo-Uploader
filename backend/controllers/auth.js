9f81896844fd549f22d0ea33ff2f26066f8f8644
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-02-15 20:11:26 -0500

    Login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -27,6 +28,42 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {
-  res.send("hey");
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "required username and password" });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("name email password");
+
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "wrong password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, name: user.name },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    return res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        path: "/",
+        expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      })
+      .status(200)
+      .json({ name: user.name, email: user.email, message: "login success" });
+  } catch (error) {
+    res.json(error);
+  }
 };
