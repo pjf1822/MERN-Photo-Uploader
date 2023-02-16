02914a5750b5f9cf825544b2c4ca3195e48aeb27
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-02-15 22:33:11 -0500

    User controllers

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,9 +3,30 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import createError from "../utils/createError.js";
 
-export const updateUser = async (req, res, next) => {};
+export const updateUser = async (req, res, next) => {
+  // pull info from request
+  const { id } = req.user.id;
+  const { name, email } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+      },
+      {
+        new: true,
+      }
+    ).select("name email");
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    return next(err);
+  }
+};
 
 export const getUserInfo = async (req, res, next) => {
+  // get id from the cookies
   const { id } = req.user.id;
 
   try {
