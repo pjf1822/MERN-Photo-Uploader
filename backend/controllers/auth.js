6bf2885181f0abb9f9538a8e8fd99eba6670f12a
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-02-15 20:53:31 -0500

    Added annotations

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,20 +1,29 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
 export const register = async (req, res) => {
+  // assign req.body
   const { username, email, password } = req.body;
 
+  // all fields must be filled out
   if (!username || !email || !password) {
-    res.status(400).json("all field names are required");
+    return next(
+      createError({
+        message: "Name, Email & password are required",
+        statusCode: 400,
+      })
+    );
   }
 
+  // does the user already exist
   const userExists = await User.findOne({ email });
 
   if (userExists) {
     res.status(400).send("this user already exists");
   }
-
+  // create hashedpassword and store user
   try {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -24,29 +33,39 @@ export const register = async (req, res) => {
 
     return res.status(201).json({ message: "good job" });
   } catch (error) {
-    res.status(400).json({ message: error });
+    return next(err);
   }
 };
 
 export const login = async (req, res, next) => {
+  // assign req.body
   const { email, password } = req.body;
+
+  // all fields must be filled out
   if (!email || !password) {
     return res.status(400).json({ message: "required username and password" });
   }
 
+  // check if user exists
   try {
     const user = await User.findOne({ email }).select("name email password");
 
     if (!user) {
-      return res.status(404).json({ message: "user not found" });
+      return next(
+        createError({ status: 404, message: "User not found with the email" })
+      );
     }
 
+    // check if password is correct
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
     if (!isPasswordCorrect) {
-      return res.status(400).json({ message: "wrong password" });
+      return next(
+        createError({ status: 400, message: "Password is incorrect" })
+      );
     }
 
+    // create jsonwebtoken for the cookie
     const token = jwt.sign(
       { id: user._id, name: user.name },
       process.env.JWT_SECRET,
@@ -54,7 +73,7 @@ export const login = async (req, res, next) => {
         expiresIn: "1d",
       }
     );
-
+    // assign the cookie and return the status/json
     return res
       .cookie("access_token", token, {
         httpOnly: true,
@@ -64,6 +83,6 @@ export const login = async (req, res, next) => {
       .status(200)
       .json({ name: user.name, email: user.email, message: "login success" });
   } catch (error) {
-    res.json(error);
+    return next(err);
   }
 };
